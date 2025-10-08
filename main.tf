aab73b8617003e8addd2ad4b40e53b81bdf22a5b
Karl Weinmeister <kweinmeister@google.com>
2025-10-08 06:13:45 -0500

    chore: update region and provider

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 7.5.0"
+      version = "~> 7.6.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
